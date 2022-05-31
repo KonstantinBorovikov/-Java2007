9dbc60220da03e5fb5ca1ec4ceeb27720a67ed04
KonstantinBorovikov <96548560+KonstantinBorovikov@users.noreply.github.com>
2022-05-31 20:19:20 +0300

    DZ1

---
+++ src/test/java/Java2HomeWork_1/Cat.java
@@ -0,0 +1,7 @@
+package Java2HomeWork_1;
+
+public class Cat extends Animal {
+    public Cat(String name) {
+        super("Кот", name, 500, 20);
+    }
+}
---
+++ src/test/java/Obstacle/Obstacle.java
@@ -0,0 +1,8 @@
+package Obstacle;
+
+import Java2HomeWork_1.Competitor;
+
+public abstract class Obstacle {
+    public abstract void doIt(Competitor competitor);
+}
+
